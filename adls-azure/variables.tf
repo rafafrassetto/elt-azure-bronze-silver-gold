fcdc929099ffd120abe91ae34188f9cf4937d51d
Rafael Frassetto Pereira <88680818+rafafrassetto@users.noreply.github.com>
2025-05-29 10:05:46 -0300

    adls-azure

---
+++ adls-azure/variables.tf
@@ -0,0 +1,8 @@
+# Lista de variaveis utilizadas nos arquivos de terraform
+variable "resource_group_name" {
+  default = "learn-49fa3e35-d401-45d9-8729-ad22c290981b"
+}
+
+variable "location" {
+  default = "westus"
+}
